63123ba13937efebd33d591b2e9200af74ac8436
searce <nitinitleen1@gmail.com>
2017-04-04 18:27:39 +0530

    convolve tensorflow

---
+++ module2/con_tensorflow.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+#Building graph
+
+input = tf.Variable(tf.random_normal([1,10,10,1]))
+filter = tf.Variable(tf.random_normal([3,3,1,1]))
+op = tf.nn.conv2d(input, filter, strides=[1, 1, 1, 1], padding='VALID')
+op2 = tf.nn.conv2d(input, filter, strides=[1, 1, 1, 1], padding='SAME')
+
+#Initialization and session
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    print("Input \n")
+    print('{0} \n'.format(input.eval()))
+    print("Filter/Kernel \n")
+    print('{0} \n'.format(filter.eval()))
+    print("Result/Feature Map with valid positions \n")
+    result = sess.run(op)
+    print(result)
+    print('\n')
+    print("Result/Feature Map with padding \n")
+    result2 = sess.run(op2)
+    print(result2)
