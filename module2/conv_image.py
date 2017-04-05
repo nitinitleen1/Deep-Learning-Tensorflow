96afb91f91e212d227ca621d1ee4bd4c30619e77
searce <nitinitleen1@gmail.com>
2017-04-05 09:36:13 +0530

    convolution image

---
+++ module2/conv_image.py
@@ -0,0 +1,66 @@
+
+#Importing
+import numpy as np
+from scipy import signal
+from scipy import misc
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+### Load image of your choice on the notebook
+print("Please type the name of your test image after uploading to \
+your notebook (just drag and grop for upload. Please remember to \
+type the extension of the file. Default: bird.jpg")
+
+raw= raw_input()
+
+im = Image.open(raw)  # type here your image's name
+
+# uses the ITU-R 601-2 Luma transform (there are several 
+# ways to convert an image to grey scale)
+
+image_gr = im.convert("L")    
+print("\n Original type: %r \n\n" % image_gr)
+
+
+# convert image to a matrix with values from 0 to 255 (uint8) 
+arr = np.asarray(image_gr) 
+print("After conversion to numerical representation: \n\n %r" % arr) 
+
+### Activating matplotlib for Ipython
+%matplotlib inline
+
+### Plot image
+
+imgplot = plt.imshow(arr)
+imgplot.set_cmap('gray')  #you can experiment different colormaps (Greys,winter,autumn)
+print("\n Input image converted to gray scale: \n")
+plt.show(imgplot)
+
+kernel = np.array([
+                        [ 0, 1, 0],
+                        [ 1,-4, 1],
+                        [ 0, 1, 0],
+                                     ]) 
+
+grad = signal.convolve2d(arr, kernel, mode='same', boundary='symm')
+
+%matplotlib inline
+
+print('GRADIENT MAGNITUDE - Feature map')
+
+fig, aux = plt.subplots(figsize=(10, 10))
+aux.imshow(np.absolute(grad), cmap='gray')
+
+type(grad)
+
+grad_biases = np.absolute(grad) + 100
+
+grad_biases[grad_biases > 255] = 255
+
+%matplotlib inline
+
+print('GRADIENT MAGNITUDE - Feature map')
+
+fig, aux = plt.subplots(figsize=(10, 10))
+aux.imshow(np.absolute(grad_biases), cmap='gray')
